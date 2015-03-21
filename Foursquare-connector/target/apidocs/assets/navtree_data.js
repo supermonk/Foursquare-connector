fce4cbe5781fc3f90d2d1af42ee95b46284a94ce
supermonk <narendra.bidari@gmail.com>
2015-03-21 08:31:01 -0500

    Created Foursquare Connector

---
+++ Foursquare-connector/target/apidocs/assets/navtree_data.js
@@ -0,0 +1,13 @@
+var NAVTREE_DATA =
+[ [ "com.kss.social", "com/kss/social/package-summary.html", [ [ "Classes", null, [ [ "FoursquareConnector", "com/kss/social/FoursquareConnector.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.kss.social.adapters", "com/kss/social/adapters/package-summary.html", [ [ "Classes", null, [ [ "FoursquareConnectorCapabilitiesAdapter", "com/kss/social/adapters/FoursquareConnectorCapabilitiesAdapter.html", null, "" ], [ "FoursquareConnectorConnectionManager", "com/kss/social/adapters/FoursquareConnectorConnectionManager.html", null, "" ], [ "FoursquareConnectorConnectionManager.ConnectionKey", "com/kss/social/adapters/FoursquareConnectorConnectionManager.ConnectionKey.html", null, "" ], [ "FoursquareConnectorLifecycleAdapter", "com/kss/social/adapters/FoursquareConnectorLifecycleAdapter.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.kss.social.config", "com/kss/social/config/package-summary.html", [ [ "Classes", null, [ [ "FoursquareConnectorConfigDefinitionParser", "com/kss/social/config/FoursquareConnectorConfigDefinitionParser.html", null, "" ], [ "FoursquareConnectorNamespaceHandler", "com/kss/social/config/FoursquareConnectorNamespaceHandler.html", null, "" ], [ "SearchVenuesDefinitionParser", "com/kss/social/config/SearchVenuesDefinitionParser.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.kss.social.processors", "com/kss/social/processors/package-summary.html", [ [ "Classes", null, [ [ "SearchVenuesMessageProcessor", "com/kss/social/processors/SearchVenuesMessageProcessor.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ Foursquare-connector/target/apidocs/java/lists.js
@@ -0,0 +1,17 @@
+var DATA = [
+      { id:0, label:"com.kss.social", link:"java/com/kss/social/package-summary.html", type:"package" },
+      { id:1, label:"com.kss.social.FoursquareConnector", link:"java/com/kss/social/FoursquareConnector.html", type:"class" },
+      { id:2, label:"com.kss.social.adapters", link:"java/com/kss/social/adapters/package-summary.html", type:"package" },
+      { id:3, label:"com.kss.social.adapters.FoursquareConnectorCapabilitiesAdapter", link:"java/com/kss/social/adapters/FoursquareConnectorCapabilitiesAdapter.html", type:"class" },
+      { id:4, label:"com.kss.social.adapters.FoursquareConnectorConnectionManager", link:"java/com/kss/social/adapters/FoursquareConnectorConnectionManager.html", type:"class" },
+      { id:5, label:"com.kss.social.adapters.FoursquareConnectorConnectionManager.ConnectionKey", link:"java/com/kss/social/adapters/FoursquareConnectorConnectionManager.ConnectionKey.html", type:"class" },
+      { id:6, label:"com.kss.social.adapters.FoursquareConnectorLifecycleAdapter", link:"java/com/kss/social/adapters/FoursquareConnectorLifecycleAdapter.html", type:"class" },
+      { id:7, label:"com.kss.social.config", link:"java/com/kss/social/config/package-summary.html", type:"package" },
+      { id:8, label:"com.kss.social.config.FoursquareConnectorConfigDefinitionParser", link:"java/com/kss/social/config/FoursquareConnectorConfigDefinitionParser.html", type:"class" },
+      { id:9, label:"com.kss.social.config.FoursquareConnectorNamespaceHandler", link:"java/com/kss/social/config/FoursquareConnectorNamespaceHandler.html", type:"class" },
+      { id:10, label:"com.kss.social.config.SearchVenuesDefinitionParser", link:"java/com/kss/social/config/SearchVenuesDefinitionParser.html", type:"class" },
+      { id:11, label:"com.kss.social.processors", link:"java/com/kss/social/processors/package-summary.html", type:"package" },
+      { id:12, label:"com.kss.social.processors.SearchVenuesMessageProcessor", link:"java/com/kss/social/processors/SearchVenuesMessageProcessor.html", type:"class" },
+      { id:13, label:"search-venues", link:"mule/foursquare.html#search-venues", type:"method" }
+
+    ];
